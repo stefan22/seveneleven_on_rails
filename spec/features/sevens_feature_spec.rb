33eb2883fa4ad2762dc8011712bd8f2cddf92b23
stefan22 <stefan.emailacct@gmail.com>
2015-08-01 18:40:15 +0100

    creating a seven eleven in the back end

--- spec/features/sevens_feature_spec.rb
+++ spec/features/sevens_feature_spec.rb
@@ -9,6 +9,19 @@ feature 'sevens locations' do
     end
   end
 
+  context 'seven elevens have been added' do
+    before do
+      Seven.create(name: 'Reseda')
+    end
+
+    scenario 'should display seven elevens' do
+        visit '/sevens'
+        expect(page).to have_content 'Reseda'
+        expect(page).not_to have_content 'No sevens have been added yet'
+        expect(current_path).to eq '/sevens'
+    end
+  end
+
 
 
 
