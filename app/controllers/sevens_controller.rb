d4e318f955f7eec1b75f29cf8fbafec613054373
stefan22 <stefan.emailacct@gmail.com>
2015-08-02 13:00:09 +0100

    crud done

--- app/controllers/sevens_controller.rb
+++ app/controllers/sevens_controller.rb
@@ -7,5 +7,35 @@ class SevensController < ApplicationController
     @seven = Seven.new
   end
 
+  def create
+    @seven = Seven.create(seven_params)
+    redirect_to '/sevens'
+  end
+
+  def seven_params
+    params.require(:seven).permit(:name)
+  end
+
+  def show
+    @seven = Seven.find(params[:id])
+  end
+
+  def edit
+    @seven = Seven.find(params[:id])
+  end
+
+  def update
+    @seven = Seven.find(params[:id])
+    @seven.update(seven_params)
+    redirect_to '/sevens'
+  end
+
+  def destroy
+    @seven = Seven.find(params[:id])
+    @seven.destroy
+    flash[:notice] = "Seven deleted successfully"
+    redirect_to '/sevens'
+  end
+
 
 end
--- spec/features/sevens_feature_spec.rb
+++ spec/features/sevens_feature_spec.rb
@@ -33,6 +33,40 @@ feature 'sevens locations' do
     end
   end
 
+  context 'show seven elevens' do
+    let!(:reseda){Seven.create name: 'Reseda'}
+
+    scenario 'displays seven eleven page' do
+        visit '/sevens'
+        click_link 'Reseda'
+        expect(page).to have_content 'Reseda'
+        expect(current_path).to eq "/sevens/#{reseda.id}"
+    end
+  end
+
+  context 'update seven elevens' do
+    before { Seven.create(name:'Reseda')}
+
+    scenario 'lets user edit seven eleven' do
+      visit '/sevens'
+      click_link 'Edit Reseda'
+      fill_in 'Name', with: 'Risotto'
+      click_button 'Update Seven'
+      expect(page).to have_content 'Risotto'
+      expect(current_path).to eq '/sevens'
+    end
+  end
+
+  context 'delete seven elevens' do
+    before { Seven.create(name:'Reseda')}
+
+    scenario 'lets users delete seven elevens' do
+      visit 'sevens'
+      click_link 'Delete Reseda'
+      expect(page).not_to have_content 'Reseda'
+      expect(page).to have_content 'Seven deleted successfully'
+    end
+  end
 
 
 
