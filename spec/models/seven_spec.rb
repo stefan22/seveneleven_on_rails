995fb7e7b257854b3faa2f3c2b4f04290d03498a
stefan22 <stefan.emailacct@gmail.com>
2015-08-05 14:43:55 +0100

    unit test for model

--- spec/models/seven_spec.rb
+++ spec/models/seven_spec.rb
@@ -4,4 +4,9 @@ require 'spec_helper'
 describe Seven, type: :model do
   it { is_expected.to have_many :reviews }
 
+  it 'is not valid with a nmae of less than three characters' do
+    seven = Seven.new(name: 'Re')
+    expect(seven).not_to be_valid
+  end
+
 end
