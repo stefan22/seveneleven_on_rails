11accbce9f352031f49bdb0577b2afda9b20f4d5
stefan22 <stefan.emailacct@gmail.com>
2015-08-06 15:03:31 +0100

    user signin and should see signout link test

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -7,11 +7,28 @@ feature 'user can sign in and out' do
       expect(page).to have_link("Sign in")
       expect(page).to have_link("Sign up")
     end
-    
+
     it 'should not see a signout link' do
       visit '/'
       expect(page).not_to have_link("Sign out")
     end
+  end
+
+  context 'user signed in and in the homepage' do
+    before do
+      visit '/'
+      click_link 'Sign up'
+      fill_in 'Email', with: 'test@test.com'
+      fill_in 'Password', with: 'pass'
+      fill_in 'Password confirmation', with: 'pass'
+      click_button 'Sign up'
+    end
+
+    it 'should see signout link' do
+      visit '/'
+      expect(page).to have_link('Sign out')
+    end
+
   end
 
 
