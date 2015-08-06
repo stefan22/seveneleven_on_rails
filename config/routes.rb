cbb098a2404aac906631eceb5eafdcd189c56b19
stefan22 <stefan.emailacct@gmail.com>
2015-08-06 14:16:34 +0100

    sets root and adds config default options

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'restaurants#index'
   resources :sevens do
     resources :reviews
 
